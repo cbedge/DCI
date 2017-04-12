6582bb06bf5c49599ac602e2d6a7bcab742a793b
cbedge <cbedge@gmail.com>
2017-04-12 19:56:15 -0400

    Small changes for effeciency
    Small changes to example data

--- DCI_Example.R
+++ DCI_Example.R
@@ -1,10 +1,13 @@
 library(igraph)
 
-setwd("C:/Users/christopher.edge/Documents/GitHub/DCI/Data")
+setwd("C:/Users/christopher.edge/Documents/GitHub/DCI/Data") #Update directory to local
 
 WC <- read.csv(file = "testWC.csv", header=T) #Select file that describes network
+names(WC)
 
 WC.prep <- calc.prep(Parth = WC, Quality="Qual") #prepare the files for DCI calculation
+
+plot(WC.prep$graph, layout=layout_with_fr, vertex.size=4, vertex.label.dist=0.5, vertex.color="red", edge.arrow.size=0.5) #plot the graph
 
 WCDCI <- DCI.calc(nodefile = WC.prep$Nodes, graphfile = WC.prep$graph, mouth=6, calc.DCIs = 1) #calculate DCI
 
@@ -14,3 +17,4 @@ WCDCI$DCIs #returns DCIs values if calculated
 
 
 ToMouth <- DistToMouth(nodefile = WC.prep$Nodes, graphfile = WC.prep$graph, mouth=6) #returns the distance from all segments to the mouth
+ToMouth
